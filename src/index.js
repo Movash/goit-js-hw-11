d8371ca8bb6360aecce119d68f1e5171608d322e
Movash <126423674+Movash@users.noreply.github.com>
2023-08-04 11:47:20 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
